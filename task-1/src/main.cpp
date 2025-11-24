8d6b83c257ce555dc60f80ff54c6646401a94a90
a.lazarchuk <a.lazarchuk@g.nsu.ru>
2025-11-24 20:21:46 +0700

    неявное копирование

--- task-1/src/main.cpp
+++ task-1/src/main.cpp
@@ -2,6 +2,11 @@
 #include <iostream>
 #include <string>
 
+BitArray createBitArray() {
+    BitArray local(4, 0b1010);  // Локальный объект
+    return local;  // ← Может быть неявное копирование
+}
+
 void printBitArray(BitArray ba) {  // Неявно вызывается конструктор копирования
     std::cout << ba.to_string() << std::endl;
     // При выходе из функции вызывается деструктор для копии
@@ -17,4 +22,5 @@ int main(){
     BitArray original(15);
     BitArray copy1 = original;//неявное копирование
     example1();
+    BitArray result = createBitArray();  // ← Неявное копирование
 }
