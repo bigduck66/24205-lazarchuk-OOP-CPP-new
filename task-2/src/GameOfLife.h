d4cd7edd3c24c7887d893ce88dfb3d03ac31163d
a.lazarchuk <a.lazarchuk@g.nsu.ru>
2025-11-25 09:34:15 +0700

    gameoflife

---
+++ task-2/src/GameOfLife.h
@@ -0,0 +1,24 @@
+#ifndef GAMEOFLIFE_H
+#define GAMEOFLIFE_H
+
+#include "Universe.h"
+#include <string>
+
+class GameOfLife {
+private:
+    Universe universe;
+    bool running;
+    
+    void printUniverse() const;
+    void processCommand(const std::string& command);
+    void showHelp() const;
+
+public:
+    GameOfLife();
+    GameOfLife(const std::string& filename);
+    
+    void run();
+    void runOffline(const std::string& inputFile, const std::string& outputFile, int iterations);
+};
+
+#endif
